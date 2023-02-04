20f1a3ff01d23e5123f623bc98ff663e5f18429b
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-02-04 16:31:02 +0530

    Update E3F.py

--- E3F.py
+++ E3F.py
@@ -1,4 +1,3 @@
-#Requirements for this script: imageio and ffmpeg!
 import sys
 import os
 from itertools import islice
@@ -6,6 +5,8 @@ import shutil
 import re
 import time
 import imageio #use pip to install Imageio and Imageio-ffmpeg
+import imageio_ffmpeg
+
 print("::::::::::  ::::::::  ::::::::::")
 print(":+:        :+:    :+: :+:")
 print("+:+               +:+ +:+")
@@ -14,16 +15,20 @@ print("+#+               +#+ +#+")
 print("#+#        #+#    #+# #+#")
 print("##########  ########  ###")
 print("WELCOME!!! Get ready to mess some videos!")
+
 files = []
 secondCall = True
 vidFile = input("Enter Video Path : ")
+
 #check if video file exists
 while not os.path.exists(vidFile):
     print("No such file in the directory!")
     vidFile = input("Enter Video Path : ")
-read=imageio.get_reader(vidFile)
+    
+read = imageio.get_reader(vidFile)
 FPS = str(int(read.get_meta_data()['fps'])) #Original FPS will be output FPS
 print("Loading...")
+
 #Delete old stuff if exists
 if os.path.exists('output'):
     shutil.rmtree('output')
@@ -32,8 +37,11 @@ if os.path.exists('output'):
 os.mkdir('output')
 os.mkdir('output_2')
 os.mkdir('output_3')
+
 #Start the fun!
-os.system(f'ffmpeg -i '+vidFile+' -f image2 "output/output_%06d.bmp"')
+ffmpeg = imageio_ffmpeg.get_ffmpeg_exe()
+os.system(f'{ffmpeg} -i '+vidFile+' -f image2 "output/output_%06d.bmp"')
+
 #
 # FIRST CALL
 #
@@ -44,6 +52,7 @@ for file in islice(files, 2, None, 3):
     print('FIRST: '+file)
     os.replace('output/'+file,'output_2/'+file)
 files.clear()
+
 #
 # SECOND CALL
 #
@@ -54,6 +63,7 @@ if secondCall == True:
         print('FIRST: '+file)
         os.replace('output/'+file,'output_2/'+file)
 files.clear()
+
 #Number every 3rd frame (minus them)
 print("Step 2: Rearranging the frames!")
 for (path, dirnames, filenames) in os.walk('output_2'):
@@ -66,6 +76,7 @@ for file in files:
     else:
         os.replace('output_2/'+file,'output_3/'+'output_'+str("{0:06}".format(fileNumber-3))+'.bmp')
 files.clear()
+
 #Number every first files (plus them)
 for (path, dirnames, filenames) in os.walk('output'):
     files.extend(os.path.join(name) for name in filenames)
@@ -84,7 +95,8 @@ for (path, dirnames, filenames) in os.walk('output_3'):
 for file in files:
     os.replace('output_3/'+file,'output/'+file)
 #Extraction done
-#os.system('rename.py') no longer needed
+    
+# os.system('rename.py') no longer needed
 _src = "output/"
 _ext = ".bmp"
 for i,filename in enumerate(os.listdir(_src)):
@@ -92,9 +104,11 @@ for i,filename in enumerate(os.listdir(_src)):
         os.rename(_src+filename, 'output_2/output_' + str(i).zfill(3)+_ext)
         print(' file '+filename+' is '+str(i).zfill(3)+_ext)
 print("Step 3: Converting the video!")
+
 if os.path.exists("video.mp4"):
     os.remove("video.mp4") #remove old video.mp4 file if exists
-os.system(f'ffmpeg -i "output_2/output_%03d.bmp" -r '+FPS+' video.mp4')
+os.system(f'{ffmpeg} -i "output_2/output_%03d.bmp" -r '+FPS+' video.mp4')
+
 #the output video is corrupted so have to convert it using imageio
 reader=imageio.get_reader("video.mp4")
 outfile=vidFile[:-4]+"_E3F_output_video.mp4"
@@ -104,10 +118,12 @@ for frames in reader:
         writer.append_data(frames)
 writer.close()
 os.remove("video.mp4")
+
 #Delete these folders to save space!
 shutil.rmtree('output')
 shutil.rmtree('output_2')
 shutil.rmtree('output_3')
+
 #Done message
 print(":::::::::   ::::::::  ::::    ::: ::::::::::   :::")
 print(":+:    :+: :+:    :+: :+:+:   :+: :+:          :+:")
@@ -118,6 +134,8 @@ print("#+#    #+# #+#    #+# #+#   #+#+# #+#  ")
 print("#########   ########  ###    #### ##########   ###")
 print("Your video is baked - "+outfile)
 print("Rerun to use it again!")
+
 time.sleep(2)
-#Modified by Akascape 
-#Orignal script by Merubokkusu
+
+# Modified by Akascape 
+# Orignal script by Merubokkusu
